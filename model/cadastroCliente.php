7b7bae555fcdece0325208e283308698430497b1
Dyego-Barros <gabutdd@gmail.com>
2017-05-17 14:26:04 -0300

    casdatroCliente

--- model/cadastroCliente.php
+++ model/cadastroCliente.php
@@ -30,24 +30,30 @@ try{
 
 }  
 
-  $statement = ("INSERT INTO clientes (nome, convenio, atendimento) VALUES (:nome, :convenio, :atendimento)");
 
-    $nomeCliente =  filter_input(INPUT_GET, 'nome-cliente', FILTER_SANITIZE_STRING);
-    $convenioCliente = filter_input(INPUT_GET, 'convenio-cliente',FILTER_VALIDATE_INT);
-    $atendimentoCliente = filter_input(INPUT_GET, 'atendimento-cliente',FILTER_VALIDATE_INT);
+ $nomeCliente =  filter_input(INPUT_GET,"nome-cliente" , FILTER_SANITIZE_STRING);
+ $convenioCliente = filter_input(INPUT_GET, "convenio-cliente",FILTER_VALIDATE_INT);
+ $atendimentoCliente = filter_input(INPUT_GET, "atendimento-cliente",FILTER_VALIDATE_INT);
+    
+
+ $pdo =("insert into clientes (nome, convenio, atendimento) VALUES ('$nomeCliente',  '$convenioCliente', '$atendimentoCliente')");
+
+   
+    
     
    
     
-    if($statement->execute(':nome',$nomeCliente, ':convenio', $convenioCliente,':convenio', $atendimentoCliente)){
+    if($pdo == TRUE){
   // Definimos a mensagem de sucesso
   echo '<script>alert("Parabens! Cliente Cadastrado com sucesso!"); </script>'; 
   echo '<script> window.close(); </script>';
   
 }else{
   // Definimos a mensagem de erro
-  echo '<script>alert("0.0! Erro ao Cadastrar "); </script>'; 
+  echo '<script>alert(" Erro ao Cadastrar Cliente ! "); </script>'; 
   echo '<script> window.close(); </script>';
 }
 
+    
     
   
