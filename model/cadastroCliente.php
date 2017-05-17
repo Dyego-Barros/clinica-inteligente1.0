93aac92f15a6c3d41fcee92459db8409e42e1527
Dyego-Barros <gabutdd@gmail.com>
2017-05-17 17:00:13 -0300

    cadastroCliente2

--- model/cadastroCliente.php
+++ model/cadastroCliente.php
@@ -5,55 +5,84 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-
-  
-
-function __construct() {
-        # Informações sobre o banco de dados:
-        $db_host = "localhost:8080";
-        $db_nome = "clinica_m";
-        $db_usuario = "root";
-        $db_senha = "";
-        $db_driver = "mysql";
-
-
-try{
-     
- self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_nome", $db_usuario, $db_senha); 
-} catch (PDOException $e){
+class cadastroCliente{
     
-    echo  "<script>alert($e);</script>";   
-}
+    public $nomeCliente;
+    public $atendimentoCliente;
+    public $convenioCliente;
+    public $query;
+    
+    
+    public function conect_DB(){
+        
+ $servername = "localhost:8080";
+$username = "root";
+$password = "";
 
-
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=clinica_m", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
+     echo  "<script>alert(Conexão bem realizado com Data Base!);</script>"; 
+    }
+catch(PDOException $e){
+  echo  "<script>alert(Falha ao tentar Conectar ao Data Base!);</script>" . $e->getMessage();
 }  
-
-
- $nomeCliente =  filter_input(INPUT_GET,"nome-cliente" , FILTER_SANITIZE_STRING);
- $convenioCliente = filter_input(INPUT_GET, "convenio-cliente",FILTER_VALIDATE_INT);
- $atendimentoCliente = filter_input(INPUT_GET, "atendimento-cliente",FILTER_VALIDATE_INT);
+}
+public function setCliente($nomeCliente, $atendimentoCiente, $convenioCliente){
     
-
- $pdo =("insert into clientes (nome, convenio, atendimento) VALUES ('$nomeCliente',  '$convenioCliente', '$atendimentoCliente')");
-
-   
+    $this->nomeCliente = $nomeCliente;
+    $this->atendimentoCliente = $atendimentoCiente;
+    $this->convenicoCliente =$convenioCliente;
     
+    $this->nomeCliente = filter_input(INPUT_GET,'nome-cliente');
+    $this->atendimentoCliente = filter_input(INPUT_GET, 'atendimento-cliente');
+    $this->convenioCliente = filter_input(INPUT_GET, 'convenio-cliente');
     
-   
-    
-    if($pdo == TRUE){
-  // Definimos a mensagem de sucesso
-  echo '<script>alert("Parabens! Cliente Cadastrado com sucesso!"); </script>'; 
-  echo '<script> window.close(); </script>';
-  
-}else{
-  // Definimos a mensagem de erro
-  echo '<script>alert(" Erro ao Cadastrar Cliente ! "); </script>'; 
-  echo '<script> window.close(); </script>';
 }
 
+public function insertDbCliente($quey){
+   
+    
+    $query = "INSERT INTO clientes(nome, convenio, atenfimento) VALUES ($nomeCliente, $convenioCliente, $atendimentoCliente)";
     
     
+}
+
+public function deleteDbCliente(){
+    
+    $delete =$sql = "DELETE FROM clientes WHERE (nome, convenio, atendimento)";
+    
+}
+
+public function updateDbCliente(){
+    
+    $update = "UPDATE clientes SET nome, convenio, atendimento";
+    
+    
+}
+
+public function selectCliente(){
+    
+    $select = "SELECT id, nome, convenio, atendimento FROM xlientes";
+    
+    
+}
+
+
+
+        
+        
+    
+    
+    
+    
+    
+    
+}
+
+
+  
+
+
   
